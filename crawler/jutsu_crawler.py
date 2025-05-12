f2138945b2d59a5cc4c5f5cfa6aa729583d0568c
rmrfy <ashishpimpre.connect@gmail.com>
2025-05-13 02:03:14 +0530

    fix: jutsu crawler; add jutsus.jsonlines

--- crawler/jutsu_crawler.py
+++ crawler/jutsu_crawler.py
@@ -6,8 +6,9 @@ class BlogSpider(scrapy.Spider):
     start_urls = ['https://naruto.fandom.com/wiki/Special:BrowseData/Jutsu?limit=250&offset=0&_cat=Jutsu']
 
     def parse(self, response):
-        for href in response.css('.smw-column-list-container')[0].css("a::attr(href)").extract():
-            extracted_data = scrapy.Request("https://naruto.fandom.com"+href, callback=self.parse_jutsu)
+        for href in response.css('.smw-columnlist-container')[0].css("a::attr(href)").extract():
+            extracted_data = scrapy.Request("https://naruto.fandom.com"+href,
+                           callback=self.parse_jutsu)
             yield extracted_data
 
         for next_page in response.css('a.mw-nextlink'):
@@ -22,12 +23,23 @@ class BlogSpider(scrapy.Spider):
 
         soup = BeautifulSoup(div_html).find('div')
 
-        jutsu_type = ''
+        jutsu_type=""
         if soup.find('aside'):
             aside = soup.find('aside')
 
-            for cell in aside.find_all('div', {'class': 'pi-data'}):
+            for cell in aside.find_all('div',{'class':'pi-data'}):
                 if cell.find('h3'):
                     cell_name = cell.find('h3').text.strip()
-                    if cell_name == 'Classification':
-                        jutsu_type = cell.find('div').text().strip()
+                    if cell_name == "Classification":
+                        jutsu_type = cell.find('div').text.strip()
+
+        soup.find('aside').decompose()
+
+        jutsu_description = soup.text.strip()
+        jutsu_description = jutsu_description.split('Trivia')[0].strip()
+
+        return dict (
+            jutsu_name = jutsu_name,
+            jutsu_type = jutsu_type,
+            jutsu_description = jutsu_description
+        )
