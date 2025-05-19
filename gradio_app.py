f3a20f27b89691e56ab67c85edc905e2675374a3
rmrfy <ashishpimpre.connect@gmail.com>
2025-05-20 01:14:39 +0530

    fix: theme classifier layout

--- gradio_app.py
+++ gradio_app.py
@@ -2,7 +2,6 @@ import gradio as gr
 from theme_classifier import ThemeClassifier
 
 def get_themes(theme_list, subtitles_path, save_path):
-    print('getting themes')
     theme_list = theme_list.split(',')
     theme_classifier = ThemeClassifier(theme_list)
     output_df = theme_classifier.get_themes(subtitles_path, save_path)
@@ -22,26 +21,25 @@ def get_themes(theme_list, subtitles_path, save_path):
         tooltip=["Theme", "Score"],
         vertical=False,
         width=500,
-        height=200
+        height=260
     )
 
     return output_chart
 
 def main():
-    print('gradio main')
     with gr.Blocks() as iface:
         with gr.Row():
             with gr.Column():
                 gr.HTML("<h1>Theme Classification (Zero Shot Classifiers)</h1>")
-            with gr.Row():
-                with gr.Column():
-                    plot = gr.BarPlot()
-                with gr.Column():
-                    theme_list = gr.Textbox(label="Themes")
-                    subtitles_path = gr.Textbox(label="Subtitles or script path")
-                    save_path = gr.Textbox(label="Save Path")
-                    get_themes_button = gr.Button("Get Themes")
-                    get_themes_button.click(get_themes, inputs=[theme_list, subtitles_path, save_path], outputs=[plot])
+                with gr.Row():
+                    with gr.Column():
+                        plot = gr.BarPlot()
+                    with gr.Column():
+                        theme_list = gr.Textbox(label="Themes")
+                        subtitles_path = gr.Textbox(label="Subtitles or script path")
+                        save_path = gr.Textbox(label="Save Path")
+                        get_themes_button = gr.Button("Get Themes")
+                        get_themes_button.click(get_themes, inputs=[theme_list, subtitles_path, save_path], outputs=[plot])
     iface.launch(share=True)
 
 
